39effeeffc8d6c2bd9e2cadde1bfa6ad26bfb124
Arnab Chakraborty <arnab033chakraborty@gmail.com>
2018-03-24 20:25:04 +0530

    Minor update in posts

--- project/Forum/home/models.py
+++ project/Forum/home/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
 	user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
 	title = models.CharField(max_length=100)
-	photo = models.ImageField(blank=True)
+	photo = models.ImageField(blank=True, upload_to='posts')
 	content = models.TextField()
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 	time_now = models.DateTimeField(auto_now=True, auto_now_add=False)
